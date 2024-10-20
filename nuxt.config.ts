ce2a9044b995496f338c18bca64a508b8f53eaa8
iammuttaqi <muntasermuttaqi@gmail.com>
2024-10-20 23:19:44 +0600

    seo meta add on nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,22 @@ export default defineNuxtConfig({
   colorMode: {
     preference: 'light',
   },
+  app: {
+    head: {
+      title: 'Taqify - Your Personalized Spotify Experience',
+      meta: [
+        { property: 'title', content: 'Taqify - Your Personalized Spotify Experience' },
+        { property: 'description', content: 'Taqify - Your Personalized Spotify Experience' },
+        { property: 'ogTitle', content: 'Taqify - Your Personalized Spotify Experience' }, 
+        { property: 'ogDescription', content: 'Explore your Spotify stats and enjoy music insights tailored for you.' }, 
+        { property: 'ogImage', content: '/android-chrome-512x512.png' }, 
+        { property: 'ogUrl', content: 'https://taqify.netlify.app' }, 
+        { property: 'twitterTitle', content: 'Taqify - Your Personalized Spotify Experience' }, 
+        { property: 'twitterDescription', content: 'Dive into your favorite tracks and artists with Taqify.' }, 
+        { property: 'twitterImage', content: '/android-chrome-512x512.png' }, 
+        { property: 'twitterCard', content: 'summary_large_image ' } 
+      ]
+    },
+  },
   compatibilityDate: "2024-10-17"
 })
